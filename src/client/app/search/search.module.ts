ef2183385c8b671d04bb175835aa068294b26482
skolli853780 <som.kolli@gmail.com>
2016-10-25 12:06:17 -0400

    Redesign summary page
    ODD 256 Redesign summary page

--- src/client/app/search/search.module.ts
+++ src/client/app/search/search.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { SearchPanelComponent } from './search.component';
 import { TaxonomyListService } from '../shared/taxonomy-list/index';
+import {SelectItem,AccordionModule} from 'primeng/primeng';
+
 
 @NgModule({
-    imports: [CommonModule, SharedModule],
+    imports: [CommonModule, SharedModule, AccordionModule],
     declarations: [SearchPanelComponent],
     exports: [SearchPanelComponent],
     providers: [TaxonomyListService]
