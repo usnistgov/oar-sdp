2a7a7bfbaae9a05d446ea3723c944adc8a530210
Harold Affo <harold.affo@nist.gov>
2017-03-22 14:00:06 -0400

    Update aftercicd.sh

--- scripts/aftercicd.sh
+++ scripts/aftercicd.sh
@@ -2,4 +2,6 @@
 
 sudo chmod 755 -R /home/ubuntu/oar-docker/portal/sdp/html/
 sudo chown -R www-data:www-data /home/ubuntu/oar-docker/portal/sdp/html/
-
+sudo docker rm -f $(sudo docker ps -a | grep "sdp")
+sudo docker rmi -f $(sudo docker images -a | grep "sdp")
+sudo docker-compose up -d --build
