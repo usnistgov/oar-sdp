ec515f2984b1e50d511d90dbabe7d3bfbde54428
skolli853780 <som.kolli@gmail.com>
2017-04-10 16:33:25 -0400

    Updated RMM service address

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -2,7 +2,7 @@ import { EnvConfig } from './env-config.interface';
 
 const DevConfig: EnvConfig = {
   ENV: 'DEV',
-  API: 'http://10.200.222.250:8082/'
+  API: 'http://10.200.94.247:8082/'
 };
 
 
