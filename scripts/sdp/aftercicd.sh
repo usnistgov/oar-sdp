ab69cd0063778f8b47429dc973c1bfd50b191bd3
Harold Affo <harold.affo@nist.gov>
2017-04-12 10:44:05 -0400

    Update and rename scripts/aftercicd.sh to scripts/sdp/aftercicd.sh

--- scripts/sdp/aftercicd.sh
+++ scripts/sdp/aftercicd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo chmod 755 -R /home/ubuntu/oar-docker/portal/sdp/html/
-sudo chown -R www-data:www-data /home/ubuntu/oar-docker/portal/sdp/html/
-cd /home/ubuntu/oar-docker/portal
+sudo chmod 755 -R /home/ubuntu/oar-docker/apps/sdp/html/
+sudo chown -R www-data:www-data /home/ubuntu/oar-docker/apps/sdp/html/
+cd /home/ubuntu/oar-docker/apps/
 
 if [[ $(sudo docker ps -a | grep "sdp") ]]; then
     sudo docker rm -f $(sudo docker ps -a | grep "sdp")
@@ -11,4 +11,5 @@ if [[ $(sudo docker images -a | grep "sdp") ]]; then
    sudo docker rmi -f $(sudo docker images -a | grep "sdp")
 fi
 
+cd /home/ubuntu/oar-docker/apps/sdp/
 sudo docker-compose up -d --build
