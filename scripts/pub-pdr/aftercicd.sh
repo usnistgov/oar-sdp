c01e26c42deb3143d852b8b55256f30a0fbc159a
Harold Affo <harold.affo@nist.gov>
2017-04-18 16:34:32 -0400

    Update aftercicd.sh

--- scripts/pub-pdr/aftercicd.sh
+++ scripts/pub-pdr/aftercicd.sh
@@ -4,11 +4,11 @@ sudo chmod 755 -R /home/ubuntu/oar-docker/apps/pub-pdr/html/
 sudo chown -R www-data:www-data /home/ubuntu/oar-docker/apps/pub-pdr/html/
 cd /home/ubuntu/oar-docker/apps/
 
-if [[ $(sudo docker ps -a | grep "pubpdr") ]]; then
-    sudo docker rm -f $(sudo docker ps -a | grep "pubpdr")
+if [[ $(sudo docker ps -aqf "name=pubpdr") ]]; then
+    sudo docker rm -f $(sudo docker ps -aqf "name=pubpdr")
 fi
-if [[ $(sudo docker images -a | grep "pubpdr") ]]; then
-   sudo docker rmi -f $(sudo docker images -a | grep "pubpdr")
+if [[ $(sudo docker images pubpdr -aq) ]]; then
+   sudo docker rmi -f $(sudo docker images pubpdr -aq)
 fi
 
 cd /home/ubuntu/oar-docker/apps/pub-pdr/
