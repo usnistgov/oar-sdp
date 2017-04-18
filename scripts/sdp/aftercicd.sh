8d44f16ebf987759bbc41434033abb728aaf3f6e
Harold Affo <harold.affo@nist.gov>
2017-04-18 16:32:47 -0400

    Update aftercicd.sh

--- scripts/sdp/aftercicd.sh
+++ scripts/sdp/aftercicd.sh
@@ -4,11 +4,11 @@ sudo chmod 755 -R /home/ubuntu/oar-docker/apps/sdp/html/
 sudo chown -R www-data:www-data /home/ubuntu/oar-docker/apps/sdp/html/
 cd /home/ubuntu/oar-docker/apps/
 
-if [[ $(sudo docker ps -a | grep "sdp") ]]; then
-    sudo docker rm -f $(sudo docker ps -a | grep "sdp")
+if [[ $(sudo docker ps -aqf "name=sdp") ]]; then
+    sudo docker rm -f $(sudo docker ps -aqf "name=sdp")
 fi
-if [[ $(sudo docker images -a | grep "sdp") ]]; then
-   sudo docker rmi -f $(sudo docker images -a | grep "sdp")
+if [[ $(sudo docker images sdp -aq) ]]; then
+   sudo docker rmi -f $(sudo docker images sdp -aq)
 fi
 
 cd /home/ubuntu/oar-docker/apps/sdp/
