e337c2bcb63d5f3172cefbc57d78a6665dd13eed
deoyani <deoyani.pics@gmail.com>
2017-04-25 13:41:12 -0400

    Updated code for production build

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -3,7 +3,8 @@ import { EnvConfig } from './env-config.interface';
 const DevConfig: EnvConfig = {
   ENV: 'DEV',
   API: 'http://10.200.94.247:8082/',
-  RMMAPI: 'http://10.200.94.247:8082/oar-rmm-service/records',
+  //RMMAPI: 'http://10.200.94.247:8082/oar-rmm-service/records',
+  RMMAPI:'http://testdata.nist.gov/rmm/records',
   SDPAPI: 'http://localhost:5556/#/',
   DISTAPI: 'http://localhost:8085/oar-rmm-service/records',
   METAPI:'http://localhost:8085/oar-rmm-service/records'
