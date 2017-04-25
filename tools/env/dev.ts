2298dde09def151528009806eab26fe40d8c38cf
skolli853780 <som.kolli@gmail.com>
2017-04-25 11:50:47 -0400

    Environment variable changes

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -2,10 +2,11 @@ import { EnvConfig } from './env-config.interface';
 
 const DevConfig: EnvConfig = {
   ENV: 'DEV',
-  API: 'http://testdata.nist.gov/rmm/',
-  RMMAPI: 'http://testdata.nist.gov/rmm/records',
-  SDPAPI: 'http://10.200.222.250/',
-  PDRAPI: 'http://testdata.nist.gov/pdr/'
+  RMMAPI: 'http://testdata.nist.gov/rmm/',
+  SDPAPI: 'http://testdata.nist.gov/sdp/',
+  PDRAPI: 'http://testdata.nist.gov/pdr/',
+  DISTAPI: 'http://localhost:8085/oar-rmm-service/records',
+  METAPI:'http://localhost:8085/oar-rmm-service/records'
 };
 
 export = DevConfig;
