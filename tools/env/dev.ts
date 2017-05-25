f338aa5ca2b5c2dc66d6a07e1afba61dcbfa3d0b
deoyani <deoyani.pics@gmail.com>
2017-05-25 13:34:33 -0400

    Updated citation string, prod config, files,references section

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -5,7 +5,7 @@ const DevConfig: EnvConfig = {
   RMMAPI: 'http://testdata.nist.gov/rmm/',
   SDPAPI: 'http://testdata.nist.gov/sdp/',
   PDRAPI: 'http://testdata.nist.gov/pdr/',
-  DISTAPI: 'http://testdata.nist.gov/ds/',
+  DISTAPI: 'http://testdata.nist.gov/od/',
   METAPI: 'http://testdata.nist.gov/meta/'
 };
 
