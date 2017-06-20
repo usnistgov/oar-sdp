05c018c3ca1d624809f7465933b21d9e4c109d09
skolli853780 <som.kolli@gmail.com>
2017-06-20 14:17:24 -0400

    ODD397 - Update Landing page URL in SDP

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -4,7 +4,7 @@ const DevConfig: EnvConfig = {
   ENV: 'DEV',
   RMMAPI: 'https://testdata.nist.gov/rmm/',
   SDPAPI: 'https://testdata.nist.gov/sdp/',
-  PDRAPI: 'https://testdata.nist.gov/pdr/',
+  PDRAPI: 'https://testdata.nist.gov/od/id/',
   DISTAPI: 'https://testdata.nist.gov/od/',
   METAPI: 'https://testdata.nist.gov/meta/'
 };
