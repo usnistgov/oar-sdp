37fd2134c76e54e100472b8d7e934cb9c65c31c2
deoyani <deoyani.pics@gmail.com>
2017-06-21 16:04:42 -0400

    Added several changes with respect to the metadata

--- src/client/pdr/landing/metadata.component.ts
+++ src/client/pdr/landing/metadata.component.ts
@@ -18,54 +18,12 @@ import { Component, Input, Pipe,PipeTransform } from '@angular/core';
 export class MetadataComponent {
    @Input() recordDisplay: any[];
    
-    testString : String;
-    someHtmlCode: string = "<div><b>This is my HTML.</b></div>";
+   
    
    ngOnInit() {
-       this.testString = " <div class=\"customfieldset\"><h1><span>Legend</span></h1> Fieldset</div>";
-      // this.testString += " <p-fieldset legend=\"Test2\"> TEST 2 Here</p-fieldset>";
-      // if(Array.isArray(this.recordDisplay['components'])){
-      //   this.testString = JSON.stringify(this.recordDisplay['components']);
-      // }
-     //this.createFieldSet(this.recordDisplay);
-      //this.createDisplay(this.recordDisplay);
+      
    }
-    // <div innerHtml='{{ testString }}'></div> 
-//     <div innerHtml='{{ someHtmlCode }}'></div> 
-//  <div [sanitizeHtml]="testString"></div>
-
-//  Key: {{entry.key}}, value: {{entry.value}} Length :: {{isArray(entry.value)}} <br>
-    
-  createFieldSet( dataObj:any[] ){
-      var recordArray = this.generateArray(dataObj);
-      let fieldsKeys = Object.keys(dataObj);
-      recordArray.forEach((item, index) => {
-          this.testString += '<p-fieldset legend="fieldsKeys[index]">';
-          if( Array.isArray(recordArray[index]) ) {
-             this.testString += 'this.createKeyVal(recordArray[index])';
-          }
-          else{
-            this.testString += recordArray[index];
-          }
-        this.testString +='</p-fieldset>';
-      });   
-   }
-
-   createDisplay( dataObj:any[] ){
-      var recordArray = this.generateArray(dataObj);
-      let fieldsKeys = Object.keys(dataObj);
-      recordArray.forEach((item, index) => {
-          this.testString += '<div class="ui-g"> <div class="ui-g-2">'+fieldsKeys[index]+'</div>'
-          if( Array.isArray(recordArray[index]) ) {
-             this.testString += '<div class="ui-g-8"> **** '+this.createKeyVal(recordArray[index])+'</div>';
-             //console.log();
-          }
-          else{
-          this.testString += '<div class="ui-g-8">'+recordArray[index]+'</div>';
-          }
-        this.testString += '</div>';
-      });   
-   }
+   
   generateArray(obj){
      return Object.keys(obj).map((key)=>{ return obj[key]});
   }
@@ -79,11 +37,7 @@ export class MetadataComponent {
      let tempString ="";
      for(let v of this.testVals)
       tempString += v +",";
-    //  this.testVals.forEach((item, index) => {
-    //   tempString += '<div class="ui-g"> <div class="ui-g-2">'+this.testKeys[index]+'</div>';
-    //  tempString += '<div class="ui-g-8">'+this.testVals[index]+'</div></div>';
-    // });
-    return tempString;
+     return tempString;
   }
 
 
