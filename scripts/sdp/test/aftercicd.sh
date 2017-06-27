a046a77012e77156bbd8d7aa8f8613c7c5b5ba81
Harold Affo <harold.affo@nist.gov>
2017-06-27 11:22:09 -0400

    Update aftercicd.sh

--- scripts/sdp/test/aftercicd.sh
+++ scripts/sdp/test/aftercicd.sh
@@ -1,16 +1,2 @@
-#!/bin/bash
-
-sudo chmod 755 -R /home/ubuntu/oar-docker/apps/sdp/html/
-sudo chown -R www-data:www-data /home/ubuntu/oar-docker/apps/sdp/html/
-cd /home/ubuntu/oar-docker/apps/
-
-if [[ $(sudo docker ps -aqf "name=sdp") ]]; then
-    sudo docker rm -f $(sudo docker ps -aqf "name=sdp")
-fi
-if [[ $(sudo docker images sdp -aq) ]]; then
-   sudo docker rmi -f $(sudo docker images sdp -aq)
-fi
-
-sudo docker-compose rm -f
-sudo docker-compose build --no-cache
-sudo docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+#! /bin/bash
+sudo /home/ubuntu/oar-docker/apps/sdp/deploy-sdp-test.sh
