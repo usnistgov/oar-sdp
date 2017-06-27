d0cdf83092be344427254d2ef4bdc46b115ace56
Harold Affo <harold.affo@nist.gov>
2017-06-27 10:55:38 -0400

    Update aftercicd.sh

--- scripts/pub-pdr/dev/aftercicd.sh
+++ scripts/pub-pdr/dev/aftercicd.sh
@@ -11,4 +11,6 @@ if [[ $(sudo docker images pub-pdr -aq) ]]; then
    sudo docker rmi -f $(sudo docker images pub-pdr -aq)
 fi
 
-sudo docker-compose up -d --build
+sudo docker-compose rm -f
+sudo docker-compose build --no-cache
+sudo docker-compose up -d 
