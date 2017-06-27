28ba4c979b1f025c8a76b55ee7cdde27ad010cc7
Harold Affo <harold.affo@nist.gov>
2017-06-27 11:27:24 -0400

    Update aftercicd.sh

--- scripts/pub-pdr/dev/aftercicd.sh
+++ scripts/pub-pdr/dev/aftercicd.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
+sudo /home/ubuntu/oar-docker/apps/pub-pdr/deploy-pubpdr-dev.sh
 
-sudo chmod 755 -R /home/ubuntu/oar-docker/apps/pub-pdr/html/
-sudo chown -R www-data:www-data /home/ubuntu/oar-docker/apps/pub-pdr/html/
-cd /home/ubuntu/oar-docker/apps/
-
-if [[ $(sudo docker ps -aqf "name=pub-pdr") ]]; then
-    sudo docker rm -f $(sudo docker ps -aqf "name=pub-pdr")
-fi
-if [[ $(sudo docker images pub-pdr -aq) ]]; then
-   sudo docker rmi -f $(sudo docker images pub-pdr -aq)
-fi
-
-sudo docker-compose rm -f
-sudo docker-compose build --no-cache
-sudo docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
