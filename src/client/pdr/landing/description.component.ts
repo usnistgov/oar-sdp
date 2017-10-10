4425f90cb4325dbddb44c5bdc39fed8107da581d
deoyani <deoyani.pics@gmail.com>
2017-10-09 22:12:08 -0400

    Updated changes to make sure research topics and keywords are not shown if not available.

--- src/client/pdr/landing/description.component.ts
+++ src/client/pdr/landing/description.component.ts
@@ -13,14 +13,14 @@ import { TreeModule,TreeNode, Tree, MenuItem } from 'primeng/primeng';
             <div id="recordDescription" class="well welldesc">
                 {{ record["description"] }}
             </div>
-            <div *ngIf="record['topic']">
+            <div *ngIf="checkTopics()">
             <strong>Research Topics:</strong>
             <span  *ngFor="let topic of record['topic']; let i =index">
                 {{ topic.tag }}
                 <span *ngIf="i < record['topic'].length-1 ">,</span>
             </span>
             </div>
-            <div *ngIf="record['keyword']">
+            <div *ngIf="checkKeywords()">
                 <b>Subject Keywords:</b>
                 <span *ngFor="let keyword of record['keyword']; let i =index">
                     {{ keyword }}<span *ngIf="i < record['keyword'].length-1 ">,</span>
@@ -112,6 +112,29 @@ checkReferences(){
               if(ref.refType === 'isDocumentedBy') return true;
           }
       }
+ }
+
+ checkKeywords(){
+    if(Array.isArray(this.record['keyword']) ){
+        if(this.record['keyword'].length > 0)
+            return true;
+        else 
+            return false;    
+    }
+    else {
+        return false;
+    }
+ }
+ checkTopics(){
+    if(Array.isArray(this.record['topic']) ){
+        if(this.record['topic'].length > 0)
+            return true;
+        else 
+            return false;    
+    }
+    else {
+        return false;
+    }
  }
 
 }
