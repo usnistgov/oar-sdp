9cfda08caeb0759cd1dcee1a77de46690b2eb770
deoyani <deoyani.pics@gmail.com>
2017-11-14 15:49:37 -0500

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -25,7 +25,8 @@ const config = {
   multicapabilities: [{
     browserName: 'chrome',
     chromeOptions: {
-      args: ["--disable-web-security","--user-data-dir=~/.e2e-chrome-profile"]
+         args: ["--headless", "--disable-gpu", "--window-size=800x600" ,
+      "--disable-web-security","--user-data-dir=~/.e2e-chrome-profile"]
      }
   }],
 
