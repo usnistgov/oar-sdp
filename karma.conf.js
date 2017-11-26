15c64589159f487543912cb78a0c404fd51b1fe8
Ray Plante <raymond.plante@nist.gov>
2017-11-26 17:05:01 -0500

    to get unit tests to run non-interactively, ensure --headless browser option is used by invoking custom launcher

--- karma.conf.js
+++ karma.conf.js
@@ -108,7 +108,7 @@ module.exports = function (config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-      'Chrome'
+      'Chrome_travis_ci'
     ],
 
 
