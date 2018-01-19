e2d5291dc8b511184d6eae99b9fb939f7c4924d4
usnistgov <som.kolli@gmail.com>
2018-01-18 21:38:01 -0500

    Datacart
    datacart component

--- src/client/pdr/datacart/datacart.component.ts
+++ src/client/pdr/datacart/datacart.component.ts
@@ -3,21 +3,21 @@ import {Http, Headers, RequestOptions, Response, ResponseContentType, URLSearchP
 import 'rxjs/add/operator/map';
 import { Subscription } from 'rxjs/Subscription';
 import { Message } from 'primeng/components/common/api';
-import { TreeModule,TreeNode, Tree, MenuItem,OverlayPanelModule,
+import { TreeTableModule,TreeNode, MenuItem,OverlayPanelModule,
   FieldsetModule,PanelModule,ContextMenuModule,
   MenuModule } from 'primeng/primeng';
 import { CartService } from './cart.service';
 import { CartEntity } from './cart.entity';
 import { Observable } from 'rxjs/Observable';
-import {ProgressSpinnerModule} from 'primeng/primeng';
+import {ProgressSpinnerModule, DialogModule} from 'primeng/primeng';
+import * as _ from 'lodash';
+import * as __ from 'underscore';
+import { environment } from '../environment';
 
-
-//import * as jsPDF  from 'jspdf';
 
 declare var Ultima: any;
-declare var jQuery: any;
 declare var saveAs: any;
-
+declare var $: any;
 
 @Component ({
   moduleId: module.id,
@@ -32,44 +32,53 @@ export class DatacartComponent implements OnInit, OnDestroy {
   layoutMode: string = 'horizontal';
   profileMode: string = 'inline';
   msgs: Message[] = [];
-  exception : string;
+  exception: string;
   errorMsg: string;
   status: string;
   errorMessage: string;
   errorMessageArray: string[];
   //searchResults: any[] = [];
-  searchValue:string;
-  recordDisplay:any[] = [];
-  keyword:string;
-  downloadZIPURL:string;
+  searchValue: string;
+  recordDisplay: any[] = [];
+  keyword: string;
+  downloadZIPURL: string;
   summaryCandidate: any[];
-  showSpinner : boolean = false;
+  showSpinner: boolean = false;
   findId: string;
   leftmenu: MenuItem[];
   rightmenu: MenuItem[];
   similarResources: boolean = false;
-  similarResourcesResults: any[]=[];
-  qcriteria:string = '';
+  similarResourcesResults: any[] = [];
+  qcriteria: string = '';
   selectedFile: TreeNode;
   isDOI = false;
   isEmail = false;
-  citeString:string = '';
+  citeString: string = '';
   type: string = '';
-  process : any[];
-  requestedId : string = '';
+  process: any[];
+  requestedId: string = '';
   isCopied: boolean = false;
   distdownload: string = '';
   serviceApi: string = '';
   metadata: boolean = false;
   cartEntities: CartEntity[];
   cols: any[];
-  selectedData: any[];
+  selectedData: TreeNode[];
+  dataFiles: TreeNode[] = [];
+  childNode: TreeNode = {};
+  display: boolean = true;
+  private distApi : string = environment.DISTAPI;
+
+
 
   /**
    * Creates an instance of the SearchPanel
    *
    */
-  constructor(private http: Http,private cartService: CartService) {
+  constructor(private http: Http, private cartService: CartService) {
+    this.getDataCartList();
+    this.display = true;
+
   }
 
   /**
@@ -77,11 +86,11 @@ export class DatacartComponent implements OnInit, OnDestroy {
    */
 
   getDataCartList() {
-    this.cartService.getAllCartEntities().then(function(result) {
-
+    this.cartService.getAllCartEntities().then(function (result) {
+      console.log("result" + result.length);
       this.cartEntities = result;
 
-    }.bind(this), function(err) {
+    }.bind(this), function (err) {
       alert("something went wrong while fetching the products");
     });
   }
@@ -90,29 +99,40 @@ export class DatacartComponent implements OnInit, OnDestroy {
     let downloadURL: string[];
     let fileName: string[];
     let params: URLSearchParams = new URLSearchParams();
-    let folderName:string;
+    let folderName: string;
     this.showSpinner = true;
     for (let selData of this.selectedData) {
-      folderName = selData.data['resId'].split("/")[2] + "-" + selData.data['resTitle'].substring(0,20);
-      console.log("folderName" + folderName);
-      console.log("downloadURL" + selData.data['downloadURL']);
-      console.log("fileName" + selData.data['id'] + selData.data['fileName']);
-      console.log("filePath" + selData.data['filePath']);
-      params.append('folderName', folderName);
-      params.append('downloadURL', selData.data['downloadURL']);
-      params.append('fileName', selData.data['id'] + selData.data['fileName']);
-      params.append('filePath', selData.data['filePath']);
+      if (selData.data['filePath'] != null) {
+        if (selData.data['filePath'].split(".").length > 1) {
+          console.log("resId" + selData.data['resId']);
+          console.log("filepath" + selData.data['filePath'])
+          //folderName = selData.data['resId'].split("/")[2] + "-" + selData.data['resTitle'].substring(0, 20);
+          params.append('folderName', folderName);
+          params.append('downloadURL', selData.data['downloadURL']);
+          params.append('fileName', selData.data['id'] + selData.data['fileName']);
+          params.append('filePath', selData.data['filePath']);
+          params.append('resFilePath', selData.data['resFilePath']);
+        }
+      }
     }
     this.downloadFile(params).subscribe(blob => {
-      saveAs(blob, "download.zip");this.showSpinner = false;
+        saveAs(blob, "download.zip");
+        this.showSpinner = false;
       }
     );
+  }
 
+  showLoadingSpinner() {
+    this.showSpinner = true;
+  }
+
+  hideLoadingSpinner() {
+    this.showSpinner = false;
   }
 
   downloadFile(params): Observable<Blob> {
-    let options = new RequestOptions({responseType: ResponseContentType.Blob });
-    return this.http.get("http://localhost:8083/oar-dist-service/cart?" + params, options)
+    let options = new RequestOptions({responseType: ResponseContentType.Blob});
+    return this.http.get(this.distApi + "/cart?" + params, options)
       .map(res => res.blob())
       .catch((error: any) => Observable.throw(error.json()));
   }
@@ -126,37 +146,121 @@ export class DatacartComponent implements OnInit, OnDestroy {
     // convert the map to an array
     for (let selData of this.selectedData) {
       dataId = selData.data['id'];
-
       // Filter out all cartEntities with given productId,  finally the new stuff from es6 can be used.
       this.cartEntities = this.cartEntities.filter(entry => entry.data.id != dataId);
 
       //save to localStorage
       this.cartService.saveListOfCartEntities(this.cartEntities);
     }
+    this.getDataCartList();
+    this.createDataHierarchy();
+  }
+
+  /**
+   * Removes all cartInstances that are bound to the productId given.
+   **/
+  removeItem(row:any) {
+
+    let dataId: any;
+    // convert the map to an array
+    let delRow = this.cartEntities.indexOf(row);
+    this.cartEntities.splice(delRow,1);
+    this.cartService.saveListOfCartEntities(this.cartEntities);
+    this.getDataCartList();
+    this.createDataHierarchy();
+    console.log("datafiles" + this.dataFiles.length);
+
   }
 
   /**
    * Get the params OnInit
    */
   ngOnInit() {
-    this.getDataCartList();
-    this.cols = [
-      {field: 'data.dataset', header: 'Dataset'},
-      {field: 'data.fileName', header: 'File Name'},
-      {field: 'data.fileSize', header: 'Size'},
-      {field: 'data.fileFormat', header: 'Format '},
-      {field: 'data.resId', header: 'ResId'},
-      {field: 'data.resTitle', header: 'ResTitle'}
-    ];
+    console.log("test" + this.cartEntities.length);
+    this.createDataHierarchy();
+    this.display = true;
   }
+
 
   ngOnDestroy() {
   }
 
-  /*
-  ngAfterViewInit(){
 
-    window.history.replaceState( {} , '#/id/', '/od/id/'+this.searchValue );
+  createDataHierarchy() {
+
+
+    let arrayList = this.cartEntities.reduce(function (result, current) {
+      result[current.data.resTitle] = result[current.data.resTitle] || [];
+      result[current.data.resTitle].push(current);
+      return result;
+    }, {});
+
+    this.dataFiles = [];
+    //let arrayList = this.cartEntities;
+    console.log("arraylist" + JSON.stringify(arrayList));
+    let parentObj: TreeNode = {};
+    for (var key in arrayList) {
+      let resId = key;
+      if (arrayList.hasOwnProperty(key)) {
+        parentObj = {
+          data: {
+            'resTitle': key,
+            'downloadStatus': 'no'
+          }
+        };
+        parentObj.children = [];
+        for (let fields of arrayList[key]) {
+          console.log("file path" + fields.data.filePath);
+          let fpath = fields.data.filePath.split("/");
+          if (fpath.length > 0) {
+            let child2: TreeNode = {};
+            child2.children = [];
+            let parent = parentObj;
+            let folderExists:boolean = false;
+            let folder = null;
+            for (let path in fpath) {
+                //let array = JSON.stringify(parent);
+                console.log("path" + fpath[path]);
+              child2 = this.createChildrenTree(fpath[path],fields.data.id,resId,key,fields.data.downloadURL,fields.data.filePath,fields.data.downloadStatus);
+              parent.children.push(child2);
+              parent = child2;
+              }
+            }
+          }
+        let tmp:any ={};
+        parentObj.children.forEach((o) => {
+          const path = o.data.filePath;
+          if (tmp[path]) {
+            tmp[path].children = tmp[path].children || [];// in case no children property or array exists
+            tmp[path].children.push(...o.children);
+          } else {
+            tmp[path] = o;
+          }
+
+        });
+
+        let values = Object.keys(tmp).map(key => tmp[key]);
+        parentObj.children = values;
+        this.dataFiles.push(parentObj);
+        }
+        console.log(JSON.stringify(this.dataFiles));
+    }
   }
-*/
+
+  createChildrenTree(path: string,id:string,resId:string,resTitle:string,downloadURL:string,resFilePath:string,downloadStatus:string){
+   let child1:TreeNode = {};
+   child1 = {
+      data: {
+          'filePath': path,
+          'id' : id,
+          'resId' : resId,
+          'resTitle': path,
+          'downloadURL' : downloadURL,
+          'resFilePath' : resFilePath,
+          'downloadStatus' : downloadStatus
+        }
+    };
+    child1.children = [];
+    return child1;
+  }
 }
