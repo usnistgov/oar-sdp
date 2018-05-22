a2ce669734551368c27a1e218d338baad7884a2c
Ray Plante <raymond.plante@nist.gov>
2018-05-22 17:03:40 -0400

    size rendering: use only 1 digit right of the decimal if the val >= 10

--- src/client/pdr/landing/filedetails.component.ts
+++ src/client/pdr/landing/filedetails.component.ts
@@ -16,16 +16,22 @@ export class FileDetailsComponent {
        //alert("download here");
    }
   
-  /**
-   * Function to display bytes in appropriate format.
-   **/ 
-  formatBytes(bytes,numAfterDecimal){
-       if(0==bytes)return"0 Bytes";
-       var base=1000,
-       d=numAfterDecimal||2,
-       e=["Bytes","kB","MB","GB","TB","PB","EB","ZB","YB"],
-       f=Math.floor(Math.log(bytes)/Math.log(base));
-       return (bytes/Math.pow(base,f)).toFixed(d)+" "+e[f]
+   /**
+    * Function to display bytes in appropriate format.
+    **/ 
+   formatBytes(bytes, numAfterDecimal) {
+       if (0==bytes) return"0 Bytes";
+        
+       var base = 1000,
+           e=["Bytes","kB","MB","GB","TB","PB","EB","ZB","YB"],
+           d = numAfterDecimal||1,
+           f = Math.floor(Math.log(bytes)/Math.log(base));
+           
+       var v = bytes/Math.pow(base,f);
+       if (numAfterDecimal == null && v < 10.0)
+           d = 2;
+
+       return v.toFixed(d)+" "+e[f];
    }
 
    addtoCart(){
