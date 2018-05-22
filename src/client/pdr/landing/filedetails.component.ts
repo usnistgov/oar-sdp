c1d532849c6272a1b41c3a4988885b7620b5591e
deoyani <deoyani.pics@gmail.com>
2018-05-22 12:35:11 -0400

    Fixed the issue of file size display. Added appropriate units after formatting bytes.

--- src/client/pdr/landing/filedetails.component.ts
+++ src/client/pdr/landing/filedetails.component.ts
@@ -15,6 +15,18 @@ export class FileDetailsComponent {
        window.open(this.fileDetails["downloadURL"]);
        //alert("download here");
    }
+  
+  /**
+   * Function to display bytes in appropriate format.
+   **/ 
+  formatBytes(a,b){
+       if(0==a)return"0 Bytes";
+       var c=1024,
+       d=b||2,
+       e=["Bytes","kB","MB","GB","TB","PB","EB","ZB","YB"],
+       f=Math.floor(Math.log(a)/Math.log(c));
+       return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]
+   }
 
    addtoCart(){
       alert("Coming soon");
