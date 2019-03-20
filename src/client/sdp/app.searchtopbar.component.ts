4b6c11f3db930652b66efe120189c67854853405
chuanlin2018 <c.lin@prometheuscomputing.com>
2019-03-20 15:21:43 -0400

    Changed version to 1.1.0 and footer logo link

--- src/client/sdp/app.searchtopbar.component.ts
+++ src/client/sdp/app.searchtopbar.component.ts
@@ -17,7 +17,7 @@ import * as _ from 'lodash';
             <span class="Fleft" style="font-weight:bold;line-height: 16.5px;display: table-cell;vertical-align: text-top;color: #FFFFFF;font-size:14px;padding-left: 2%">SCIENCE <br> DATA PORTAL </span>
           </a>
       </span>
-      <span class="badge" style="background-color:#982800;vertical-align: text-top;margin-top: 10px;">1.1.0-beta</span>
+      <span class="badge" style="background-color:#982800;vertical-align: text-top;margin-top: 10px;">1.1.0</span>
         <div class="topbar-right">
               <a id="menu-button" href="#" (click)="app.onMenuButtonClick($event)">
                     <i></i>
