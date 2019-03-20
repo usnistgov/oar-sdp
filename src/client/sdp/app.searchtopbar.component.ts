c87d53e9c6de4ca0886d104a655521bab0426647
chuanlin2018 <c.lin@prometheuscomputing.com>
2019-03-20 16:12:56 -0400

    Changed version to 1.1.7 and color

--- src/client/sdp/app.searchtopbar.component.ts
+++ src/client/sdp/app.searchtopbar.component.ts
@@ -17,7 +17,7 @@ import * as _ from 'lodash';
             <span class="Fleft" style="font-weight:bold;line-height: 16.5px;display: table-cell;vertical-align: text-top;color: #FFFFFF;font-size:14px;padding-left: 2%">SCIENCE <br> DATA PORTAL </span>
           </a>
       </span>
-      <span class="badge" style="background-color:#982800;vertical-align: text-top;margin-top: 10px;">1.1.0</span>
+      <span class="badge" style="color:black;background-color:#f0f0f0;vertical-align: text-top;margin-top: 10px;">1.1.7</span>
         <div class="topbar-right">
               <a id="menu-button" href="#" (click)="app.onMenuButtonClick($event)">
                     <i></i>
