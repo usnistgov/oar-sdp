ba4bc2d28eaea9b58b46a26a4f26645f34012c37
chuanlin2018 <c.lin@prometheuscomputing.com>
2019-06-05 15:21:51 -0400

    Added Google Analytics script

--- angular/src/app/about/about.component.ts
+++ angular/src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var gas:Function;
+
 @Component({
   selector: 'sdp-about',
   templateUrl: './about.component.html',
@@ -10,6 +12,7 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    gas('create', 'pageview');
   }
 
 }
