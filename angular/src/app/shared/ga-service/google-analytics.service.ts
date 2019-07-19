9f9de0e8d6a9a02303b590730dc5f1d6f5ef742c
chuanlin2018 <c.lin@prometheuscomputing.com>
2019-07-19 12:05:07 -0400

    fixed dup page views

--- angular/src/app/shared/ga-service/google-analytics.service.ts
+++ angular/src/app/shared/ga-service/google-analytics.service.ts
@@ -1,13 +1,19 @@
+// Google Analytics (GA) script was handled differently from traditional web pages. Angular 2+ is a SPA
+// which was considered only one page.
+// When GA script was first added to index.html, it analyzed index.html page to decide what element to listen.
+// In this all, index.html is very simple. There is nothing to listen but the home route '/'. 
+// So when home route is added to the page it later it will trigger this router event. 
+// We need to skip '/' to avoid registaer dup listeners.
+
 import { Injectable } from '@angular/core';
 import {Router, NavigationEnd} from '@angular/router';
-import { environment } from '../../../environments/environment';
 declare var gas:Function; 
 
 @Injectable()
 export class GoogleAnalyticsService {
   constructor(router: Router) {
     router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
+      if (event instanceof NavigationEnd && event.url != '/') {
         setTimeout(() => {
           gas('send', 'pageview', event.url, 'pageview');
         }, 1000);
