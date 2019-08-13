8d500815abc8b59965f28a6b61ef20d51923d4e2
chuanlin2018 <c.lin@prometheuscomputing.com>
2019-08-13 11:49:31 -0400

    fixed test code

--- angular/src/app/about/about.component.spec.ts
+++ angular/src/app/about/about.component.spec.ts
@@ -3,6 +3,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AboutComponent } from './about.component';
 import { GoogleAnalyticsService } from "../shared/ga-service/google-analytics.service";
 import { GoogleAnalyticsServiceMock } from "../shared/ga-service/google-analytics.service.mock";
+import { CommonService } from '../shared/common/common.service';
+import { RouterModule } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -10,8 +14,9 @@ describe('AboutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterModule, RouterTestingModule],
       declarations: [ AboutComponent ],
-      providers: [{provide: GoogleAnalyticsService, useClass: GoogleAnalyticsServiceMock}]
+      providers: [{provide: GoogleAnalyticsService, useClass: GoogleAnalyticsServiceMock}, CommonService]
     })
     .compileComponents();
   }));
--- angular/src/app/shared/common/common.service.spec.ts
+++ angular/src/app/shared/common/common.service.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { CommonService } from './common.service';
+import { GoogleAnalyticsService } from '../../shared/ga-service/google-analytics.service';
+import { RouterModule } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CommonService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CommonService]
+      imports: [HttpClientTestingModule, RouterModule, RouterTestingModule],
+      providers: [CommonService, GoogleAnalyticsService]
     });
   });
 
