9ee317a2286c747c2e0dbeb29bb6ca456f708a55
RayPlante <raymond.plante@nist.gov>
2024-08-16 17:11:48 -0400

    make room for redesigned sdp code

--- angular/src/app/shared/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This barrel file provides the exports for the shared resources (services, components).
- */
-export * from '../frame/footbar/index';
-export * from './search-service/index';
-export * from './taxonomy-list/index';
-export * from './searchfields-list/index';
-export * from './search-query/index';
-
--- angular/src/app/shared/searchfields-list/index.ts
+++
@@ -1,4 +0,0 @@
-/**
- * This barrel file provides the export for the shared Advanced Search Fields list.
- */
-export * from './searchfields-list.service';
