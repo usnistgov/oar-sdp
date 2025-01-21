fe9c23219377a6c753142133fd8cdd57aa329c8a
Mehdi Shahid <github@mehdishahid.com>
2025-01-21 15:23:56 -0500

    test: fix tests to pass on angular18 with prime16.9

--- angular/src/app/policy/policy.component.spec.ts
+++ angular/src/app/policy/policy.component.spec.ts
@@ -1,29 +1,36 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 
-import { PolicyComponent } from './policy.component';
+import { PolicyComponent } from "./policy.component";
 import { GoogleAnalyticsService } from "../shared/ga-service/google-analytics.service";
 import { GoogleAnalyticsServiceMock } from "../shared/ga-service/google-analytics.service.mock";
-import { CommonService } from '../shared/common/common.service';
-import { RouterModule } from '@angular/router';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
-describe('PolicyComponent', () => {
+import { CommonService } from "../shared/common/common.service";
+import { RouterModule } from "@angular/router";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from "@angular/common/http";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+describe("PolicyComponent", () => {
   let component: PolicyComponent;
   let fixture: ComponentFixture<PolicyComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-    declarations: [PolicyComponent],
-    imports: [RouterModule, RouterTestingModule],
-    providers: [
-        { provide: GoogleAnalyticsService, useClass: GoogleAnalyticsServiceMock }, CommonService,
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [PolicyComponent],
+      imports: [RouterModule, RouterTestingModule],
+      providers: [
+        {
+          provide: GoogleAnalyticsService,
+          useClass: GoogleAnalyticsServiceMock,
+        },
+        CommonService,
         provideHttpClient(withInterceptorsFromDi()),
-        provideHttpClientTesting()
-    ]
-})
-    .compileComponents();
+        provideHttpClientTesting(),
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -32,7 +39,7 @@ describe('PolicyComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
