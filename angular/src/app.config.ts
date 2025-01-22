48857ba7a9bb1e1d0130a7b452b490c714e0076f
Mehdi Shahid <github@mehdishahid.com>
2025-01-22 11:28:12 -0500

    fix: primeng/config was not needed

--- angular/src/app.config.ts
+++ angular/src/app.config.ts
@@ -1,13 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideAnimationsAsync(),
-        providePrimeNG({ 
-            theme: {
-            }
-        })
+        provideAnimationsAsync()
     ]
 };
